c971029dd221bcd294e6bbe6a8617c6a49025c0b
JESUSHMSIS <jeshuanaco7@gmail.com>
2024-01-26 19:59:47 -0400

    first commit

---
+++ 001/index.js
@@ -0,0 +1,16 @@
+//estos son algunas funcionalidades que tiene javascript
+
+// console.log("hello world");
+// console.log("i like to pizza")
+// window.alert("this is a alert")
+// window.alert("I like pizza")
+
+//Esto es un comentario en javascript
+
+/* esto es 
+un comentario 
+global
+*/
+
+document.getElementById("myH1").textContent = "Hola desde javascript"
+document.getElementById("myP1").textContent = "Este es un texto añadido desde javascript"
